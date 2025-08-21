4ce0881c0b8cb3049d0f40e83eb99589e9c5ae62
Hanny <semethanny@gmail.com>
2025-08-21 12:20:53 +0800

    gfdg

--- ckanext/datopian/plugin.py
+++ ckanext/datopian/plugin.py
@@ -11,6 +11,8 @@ class DatopianPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
+        toolkit.add_resource('assets',
+                             'datopian')
 
     # IBlueprint
     def get_blueprint(self):
